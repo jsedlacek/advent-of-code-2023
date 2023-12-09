7d4a36d5d436898fe48f407a9c151f9a6af0f83d
Jakub Sedlacek <jakub.sedlacek@productboard.com>
2023-12-09 16:22:33 +0100

    handle errors in Game

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -8,20 +8,13 @@ impl Game {
     fn parse(input: &str) -> Result<Self, ParseIntError> {
         let inputs = input
             .lines()
-            .map(|l| {
-                l.split(" ")
-                    .map(|v| {
-                        let r = v.parse();
-                        r
-                    })
-                    .collect()
-            })
+            .map(|l| l.split(' ').map(|v| v.parse()).collect())
             .collect::<Result<Vec<_>, _>>()?;
 
         Ok(Self { inputs })
     }
 
-    fn diff(sequence: &Vec<i64>) -> Vec<i64> {
+    fn diff(sequence: &[i64]) -> Vec<i64> {
         sequence
             .windows(2)
             .map(|window| {
@@ -34,27 +27,31 @@ impl Game {
             .collect()
     }
 
-    fn next_prediction(sequence: &Vec<i64>) -> i64 {
+    fn next_prediction(sequence: &[i64]) -> Result<i64, String> {
         if sequence.iter().all(|&i| i == 0) {
-            return 0;
+            return Ok(0);
         }
 
-        sequence.last().unwrap() + Self::next_prediction(&Self::diff(sequence))
+        Ok(
+            sequence.last().ok_or("Empty sequence")?
+                + Self::next_prediction(&Self::diff(sequence))?,
+        )
     }
 
-    fn prev_prediction(sequence: &Vec<i64>) -> i64 {
+    fn prev_prediction(sequence: &[i64]) -> Result<i64, String> {
         if sequence.iter().all(|&i| i == 0) {
-            return 0;
+            return Ok(0);
         }
 
-        sequence.first().unwrap() - Self::prev_prediction(&Self::diff(sequence))
+        Ok(sequence.first().ok_or("Empty sequence")?
+            - Self::prev_prediction(&Self::diff(sequence))?)
     }
 
-    fn part1(&self) -> i64 {
+    fn part1(&self) -> Result<i64, String> {
         self.inputs.iter().map(|i| Self::next_prediction(i)).sum()
     }
 
-    fn part2(&self) -> i64 {
+    fn part2(&self) -> Result<i64, String> {
         self.inputs.iter().map(|i| Self::prev_prediction(i)).sum()
     }
 }
@@ -62,20 +59,20 @@ impl Game {
 fn main() {
     let game = Game::parse(include_str!("input.txt")).unwrap();
 
-    dbg!(game.part1());
-    dbg!(game.part2());
+    dbg!(game.part1().unwrap());
+    dbg!(game.part2().unwrap());
 }
 
 #[test]
 fn part1() {
     let game = Game::parse(include_str!("sample-input.txt")).unwrap();
 
-    assert_eq!(game.part1(), 114);
+    assert_eq!(game.part1(), Ok(114));
 }
 
 #[test]
 fn part2() {
     let game = Game::parse(include_str!("sample-input.txt")).unwrap();
 
-    assert_eq!(game.part2(), 2);
+    assert_eq!(game.part2(), Ok(2));
 }
