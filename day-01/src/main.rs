6c288869418db58cea6ee32e8299498282dda1e3
Jakub Sedlacek <jakub.sedlacek@productboard.com>
2023-12-14 13:08:09 +0100

    error handling and clippy

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,81 +1,88 @@
-use std::{collections::HashMap, convert::identity};
+use std::collections::HashMap;
 
-const SAMPLE_INPUT: &str = include_str!("sample-input.txt");
-const SAMPLE_INPUT_2: &str = include_str!("sample-input-2.txt");
-const INPUT: &str = include_str!("input.txt");
+use anyhow::{anyhow, Result};
 
-fn find_first(haystack: &str, map: &HashMap<String, u32>) -> u32 {
-    *map.iter()
-        .map(|(key, value)| {
-            let pos = haystack.find(key);
-
-            return pos.map(|p| (p, value));
-        })
-        .filter_map(identity)
-        .min_by_key(|(pos, _)| *pos)
-        .unwrap()
-        .1
+fn find_first(haystack: &str, map: &HashMap<String, u32>) -> Result<u32> {
+    Ok(*map
+        .iter()
+        .filter_map(|(key, value)| haystack.find(key).map(|p| (p, value)))
+        .min_by_key(|&(pos, _)| pos)
+        .ok_or(anyhow!("Key not found"))?
+        .1)
 }
 
-fn find_last(haystack: &str, map: &HashMap<String, u32>) -> u32 {
-    *map.iter()
-        .map(|(key, value)| {
-            let pos = haystack.rfind(key);
-
-            return pos.map(|p| (p, value));
-        })
-        .filter_map(identity)
+fn find_last(haystack: &str, map: &HashMap<String, u32>) -> Result<u32> {
+    Ok(*map
+        .iter()
+        .filter_map(|(key, value)| haystack.rfind(key).map(|p| (p, value)))
         .max_by_key(|(pos, _)| *pos)
-        .unwrap()
-        .1
+        .ok_or(anyhow!("Key not found"))?
+        .1)
 }
 
-fn process(input: &str, map: &HashMap<String, u32>) -> u32 {
-    input
+fn process(input: &str, map: &HashMap<String, u32>) -> Result<u32> {
+    Ok(input
         .lines()
-        .map(|line| {
-            let first = find_first(line, map);
-            let last = find_last(line, map);
+        .map(|line| -> Result<u32> {
+            let first = find_first(line, map)?;
+            let last = find_last(line, map)?;
 
-            format!("{first}{last}").parse::<u32>().unwrap()
+            Ok(format!("{first}{last}").parse::<u32>()?)
         })
-        .sum()
+        .collect::<Result<Vec<_>>>()?
+        .into_iter()
+        .sum())
 }
 
-fn process_part_1(input: &str) -> u32 {
+fn process_part_1(input: &str) -> Result<u32> {
     let mut map = HashMap::new();
 
     for i in 1..=9 {
         map.insert(i.to_string(), i);
     }
 
-    process(&input, &map)
+    process(input, &map)
 }
 
-fn process_part_2(input: &str) -> u32 {
-    let mut map = HashMap::new();
+fn process_part_2(input: &str) -> Result<u32> {
+    let words = [
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .iter()
+    .map(|&(s, n)| (s.to_string(), n));
 
-    map.insert("one".to_string(), 1);
-    map.insert("two".to_string(), 2);
-    map.insert("three".to_string(), 3);
-    map.insert("four".to_string(), 4);
-    map.insert("five".to_string(), 5);
-    map.insert("six".to_string(), 6);
-    map.insert("seven".to_string(), 7);
-    map.insert("eight".to_string(), 8);
-    map.insert("nine".to_string(), 9);
+    let numbers = (1..=9).map(|n| (n.to_string(), n));
 
-    for i in 1..=9 {
-        map.insert(i.to_string(), i);
-    }
+    let map = words.chain(numbers).collect::<HashMap<_, _>>();
 
-    process(&input, &map)
+    process(input, &map)
 }
 
-fn main() {
-    assert_eq!(process_part_1(SAMPLE_INPUT), 142);
-    assert_eq!(process_part_1(INPUT), 54239);
+fn main() -> Result<()> {
+    println!("Part 1: {}", process_part_1(include_str!("input.txt"))?);
+    println!("Part 2: {}", process_part_2(include_str!("input.txt"))?);
 
-    dbg!(process_part_2(SAMPLE_INPUT_2));
-    dbg!(process_part_2(INPUT));
+    Ok(())
+}
+
+#[test]
+fn part1() -> Result<()> {
+    assert_eq!(process_part_1(include_str!("sample-input.txt"))?, 142);
+
+    Ok(())
+}
+
+#[test]
+fn part2() -> Result<()> {
+    assert_eq!(process_part_2(include_str!("sample-input-2.txt"))?, 281);
+
+    Ok(())
 }
