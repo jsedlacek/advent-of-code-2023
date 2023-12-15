48b47e9c7d02f732d42e4d9858fa594ae0cf715b
Jakub Sedlacek <jakub.sedlacek@productboard.com>
2023-12-15 17:54:49 +0100

    make card parsing generic
    instead of dynamic time logic via version field

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,9 +1,13 @@
 use std::collections::HashMap;
 
 use nom::{
-    character::complete::{one_of, space1},
-    character::{complete::newline, complete::u64},
-    combinator::map,
+    branch::alt,
+    bytes::complete::tag,
+    character::{
+        complete::newline,
+        complete::{space1, u64},
+    },
+    combinator::{map, value},
     multi::{many_m_n, separated_list0},
     sequence::tuple,
     IResult,
@@ -11,10 +15,8 @@ use nom::{
 
 use anyhow::Result;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
-enum GameVersion {
-    V1,
-    V2,
+trait Parser<T> {
+    fn parse(input: &str) -> IResult<&str, T>;
 }
 
 struct Game {
@@ -22,14 +24,8 @@ struct Game {
 }
 
 impl Game {
-    fn parse_1(input: &str) -> IResult<&str, Self> {
-        map(separated_list0(newline, Round::parse_1), |rounds| Self {
-            rounds,
-        })(input)
-    }
-
-    fn parse_2(input: &str) -> IResult<&str, Self> {
-        map(separated_list0(newline, Round::parse_2), |rounds| Self {
+    fn parse<T: Parser<Card>>(input: &str) -> IResult<&str, Self> {
+        map(separated_list0(newline, Round::parse::<T>), |rounds| Self {
             rounds,
         })(input)
     }
@@ -57,19 +53,10 @@ struct Round {
 }
 
 impl Round {
-    fn parse_1(input: &str) -> IResult<&str, Self> {
+    fn parse<T: Parser<Card>>(input: &str) -> IResult<&str, Self> {
         // 32T3K 765
-        map(tuple((Hand::parse_1, space1, u64)), |(hand, _, bid)| Self {
-            hand,
-            bid,
-        })(input)
-    }
-
-    fn parse_2(input: &str) -> IResult<&str, Self> {
-        // 32T3K 765
-        map(tuple((Hand::parse_2, space1, u64)), |(hand, _, bid)| Self {
-            hand,
-            bid,
+        map(tuple((Hand::parse::<T>, space1, u64)), |(hand, _, bid)| {
+            Self { hand, bid }
         })(input)
     }
 }
@@ -77,39 +64,29 @@ impl Round {
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Hand {
     cards: Vec<Card>,
-    version: GameVersion,
 }
 
 impl Hand {
-    fn parse_1(input: &str) -> IResult<&str, Hand> {
+    fn parse<T: Parser<Card>>(input: &str) -> IResult<&str, Self> {
         // 32T3K
-        map(many_m_n(5, 5, Card::parse_1), |cards| Self {
-            cards,
-            version: GameVersion::V1,
-        })(input)
+        map(many_m_n(5, 5, T::parse), |cards| Self { cards })(input)
     }
+}
 
-    fn parse_2(input: &str) -> IResult<&str, Hand> {
-        // 32T3K
-        map(many_m_n(5, 5, Card::parse_2), |cards| Self {
-            cards,
-            version: GameVersion::V2,
-        })(input)
-    }
-
+impl Hand {
     fn get_type(&self) -> HandType {
-        let mut card_map = HashMap::new();
+        let mut card_map: HashMap<Card, u32> = HashMap::new();
 
         for &c in &self.cards {
             card_map
-                .entry(c.0)
+                .entry(c)
                 .and_modify(|count| *count += 1)
                 .or_insert(1);
         }
 
-        if self.version == GameVersion::V2 && card_map.get(&'J').is_some() && card_map.len() > 1 {
-            let &joker_count = card_map.get(&'J').unwrap();
-            card_map.remove(&'J');
+        if card_map.get(&Card::Joker).is_some() && card_map.len() > 1 {
+            let &joker_count = card_map.get(&Card::Joker).unwrap();
+            card_map.remove(&Card::Joker);
             let (&card, &count) = card_map.iter().max_by_key(|(_, &count)| count).unwrap();
             card_map.insert(card, count + joker_count);
         }
@@ -143,17 +120,7 @@ impl Hand {
 
 impl Ord for Hand {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let self_value = HAND_TYPE_VALUES
-            .iter()
-            .position(|t| *t == self.get_type())
-            .unwrap();
-
-        let other_value = HAND_TYPE_VALUES
-            .iter()
-            .position(|t| *t == other.get_type())
-            .unwrap();
-
-        match self_value.cmp(&other_value) {
+        match self.get_type().cmp(&other.get_type()) {
             std::cmp::Ordering::Less => std::cmp::Ordering::Less,
             std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
             std::cmp::Ordering::Equal => self.cards.cmp(&other.cards),
@@ -167,72 +134,89 @@ impl PartialOrd for Hand {
     }
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 enum HandType {
-    FiveOfAKind,
-    FourOfAKind,
-    FullHouse,
-    ThreeOfAKind,
-    TwoPair,
-    OnePair,
     HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
 }
 
-const HAND_TYPE_VALUES: [HandType; 7] = [
-    HandType::HighCard,
-    HandType::OnePair,
-    HandType::TwoPair,
-    HandType::ThreeOfAKind,
-    HandType::FullHouse,
-    HandType::FourOfAKind,
-    HandType::FiveOfAKind,
-];
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Card {
+    Joker,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct Card(char, GameVersion);
+struct CardParserV1;
 
-const CARD_VALUES_1: &str = "23456789TJQKA";
-const CARD_VALUES_2: &str = "J23456789TQKA";
-
-impl Card {
-    fn parse_1(input: &str) -> IResult<&str, Self> {
-        // "T""
-
-        map(one_of(CARD_VALUES_1), |c| Self(c, GameVersion::V1))(input)
-    }
-
-    fn parse_2(input: &str) -> IResult<&str, Self> {
-        // "T""
-
-        map(one_of(CARD_VALUES_2), |c| Self(c, GameVersion::V1))(input)
-    }
-
-    fn value(self) -> Option<usize> {
-        match self.1 {
-            GameVersion::V1 => CARD_VALUES_1.find(self.0),
-            GameVersion::V2 => CARD_VALUES_2.find(self.0),
-        }
+impl Parser<Card> for CardParserV1 {
+    fn parse(input: &str) -> IResult<&str, Card> {
+        alt((
+            // value(CardValue::Joker, tag("J")),
+            // J is Card::Jack in v1
+            value(Card::Two, tag("2")),
+            value(Card::Three, tag("3")),
+            value(Card::Four, tag("4")),
+            value(Card::Five, tag("5")),
+            value(Card::Six, tag("6")),
+            value(Card::Seven, tag("7")),
+            value(Card::Eight, tag("8")),
+            value(Card::Nine, tag("9")),
+            value(Card::Ten, tag("T")),
+            value(Card::Jack, tag("J")),
+            value(Card::Queen, tag("Q")),
+            value(Card::King, tag("K")),
+            value(Card::Ace, tag("A")),
+        ))(input)
     }
 }
 
-impl Ord for Card {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.value().cmp(&other.value())
-    }
-}
+struct CardParserV2;
 
-impl PartialOrd for Card {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+impl Parser<Card> for CardParserV2 {
+    fn parse(input: &str) -> IResult<&str, Card> {
+        alt((
+            value(Card::Joker, tag("J")),
+            value(Card::Two, tag("2")),
+            value(Card::Three, tag("3")),
+            value(Card::Four, tag("4")),
+            value(Card::Five, tag("5")),
+            value(Card::Six, tag("6")),
+            value(Card::Seven, tag("7")),
+            value(Card::Eight, tag("8")),
+            value(Card::Nine, tag("9")),
+            value(Card::Ten, tag("T")),
+            // value(CardValue::Jack, tag("J"))
+            // J is Card::Joker in v2
+            value(Card::Queen, tag("Q")),
+            value(Card::King, tag("K")),
+            value(Card::Ace, tag("A")),
+        ))(input)
     }
 }
 
 fn main() -> Result<()> {
-    let (_, game1) = Game::parse_1(include_str!("input.txt"))?;
+    let (_, game1) = Game::parse::<CardParserV1>(include_str!("input.txt"))?;
 
     dbg!(game1.puzzle());
 
-    let (_, game2) = Game::parse_2(include_str!("input.txt"))?;
+    let (_, game2) = Game::parse::<CardParserV2>(include_str!("input.txt"))?;
     dbg!(game2.puzzle());
 
     Ok(())
@@ -240,7 +224,7 @@ fn main() -> Result<()> {
 
 #[test]
 fn part1() -> Result<()> {
-    let (_, game) = Game::parse_1(include_str!("sample-input.txt"))?;
+    let (_, game) = Game::parse::<CardParserV1>(include_str!("sample-input.txt"))?;
 
     assert_eq!(game.puzzle(), 6440);
 
@@ -249,9 +233,27 @@ fn part1() -> Result<()> {
 
 #[test]
 fn part2() -> Result<()> {
-    let (_, game) = Game::parse_2(include_str!("sample-input.txt"))?;
+    let (_, game) = Game::parse::<CardParserV2>(include_str!("sample-input.txt"))?;
 
     assert_eq!(game.puzzle(), 5905);
+
+    Ok(())
+}
+
+#[test]
+fn parse_v1() -> Result<()> {
+    let (_, card) = CardParserV1::parse("J")?;
+
+    assert_eq!(card, Card::Jack);
+
+    Ok(())
+}
+
+#[test]
+fn parse_v2() -> Result<()> {
+    let (_, card) = CardParserV2::parse("J")?;
+
+    assert_eq!(card, Card::Joker);
 
     Ok(())
 }
