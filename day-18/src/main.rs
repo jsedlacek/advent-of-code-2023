f78c6ca5567c3b0f1c535a0ef596316bff1c28e3
Jakub Sedlacek <jakub.sedlacek@productboard.com>
2023-12-18 20:34:52 +0100

    Update main.rs

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -6,12 +6,10 @@ use anyhow::Result;
 const INPUT: &str = include_str!("input.txt");
 
 fn main() -> Result<()> {
-    let (_, game1) = parser::v1::parse_game(INPUT)?;
-
+    let game1 = parser::v1::parse_game(INPUT)?.1;
     println!("Part 1: {}", game1.puzzle());
 
-    let (_, game2) = parser::v2::parse_game(INPUT)?;
-
+    let game2 = parser::v2::parse_game(INPUT)?.1;
     println!("Part 2: {}", game2.puzzle());
 
     Ok(())
