6d42b13db9725882040af280bd2ed7ff521de6b6
Jakub Sedlacek <jakub.sedlacek@productboard.com>
2023-12-18 18:07:12 +0100

    Update parser.rs

--- day-18/src/parser.rs
+++ day-18/src/parser.rs
@@ -4,7 +4,7 @@ use nom::{
     character::complete::{alphanumeric1, newline, space1, u64},
     combinator::{map, map_res, value},
     multi::separated_list1,
-    sequence::tuple,
+    sequence::{delimited, separated_pair, terminated, tuple},
     IResult,
 };
 
@@ -20,41 +20,33 @@ pub fn parse_game_v2(input: &str) -> IResult<&str, Game> {
 
 pub fn parse_instruction_v1(input: &str) -> IResult<&str, Instruction> {
     // Example: "R 6 (#70c710)"
+    // Ignore the hex color part
 
     map(
-        tuple((
-            parse_direction_v1,
-            space1,
-            u64,
-            space1,
-            tag("(#"),
-            alphanumeric1,
-            tag(")"),
-        )),
-        |(dir, _, steps, _, _, _, _)| Instruction::new(dir, steps),
+        terminated(
+            separated_pair(parse_direction_v1, space1, u64),
+            tuple((space1, tag("(#"), alphanumeric1, tag(")"))),
+        ),
+        |(dir, steps)| Instruction::new(dir, steps),
     )(input)
 }
 
 pub fn parse_instruction_v2(input: &str) -> IResult<&str, Instruction> {
     // Example: "R 6 (#70c710)"
+    // Take instructions from the #70c710 code
+    // - "70c71" is converted as hex into steps
+    // - "0" indicates direction
 
-    map(
-        tuple((
-            parse_direction_v1,
-            space1,
-            u64,
-            space1,
-            tag("(#"),
-            map(
-                tuple((
-                    map_res(take(5u8), |s| u64::from_str_radix(s, 16)),
-                    parse_direction_v2,
-                )),
-                |(steps, dir)| Instruction::new(dir, steps),
-            ),
-            tag(")"),
-        )),
-        |(_, _, _, _, _, ins, _)| ins,
+    delimited(
+        tuple((parse_direction_v1, space1, u64, space1, tag("(#"))),
+        map(
+            tuple((
+                map_res(take(5u8), |s| u64::from_str_radix(s, 16)),
+                parse_direction_v2,
+            )),
+            |(steps, dir)| Instruction::new(dir, steps),
+        ),
+        tag(")"),
     )(input)
 }
 
@@ -78,39 +70,61 @@ fn parse_direction_v2(input: &str) -> IResult<&str, Direction> {
 
 #[cfg(test)]
 mod tests {
-    use crate::{
-        game::{Direction, Instruction},
-        parser::{parse_game_v1, parse_instruction_v1, parse_instruction_v2},
+    use anyhow::Result;
+
+    use crate::game::{Direction, Instruction};
+
+    use super::{
+        parse_direction_v1, parse_direction_v2, parse_game_v1, parse_game_v2, parse_instruction_v1,
+        parse_instruction_v2,
     };
 
     #[test]
-    fn test_parse_game_v1() -> anyhow::Result<()> {
+    fn test_parse_game_v1() -> Result<()> {
         parse_game_v1("R 6 (#70c710)")?;
 
         Ok(())
     }
 
     #[test]
-    fn test_parse_game_v2() -> anyhow::Result<()> {
-        parse_game_v1("R 6 (#70c710)")?;
+    fn test_parse_game_v2() -> Result<()> {
+        parse_game_v2("R 6 (#70c710)")?;
 
         Ok(())
     }
 
     #[test]
-    fn test_parse_instruction_v1() -> anyhow::Result<()> {
-        let (_, instruction) = parse_instruction_v1("R 6 (#70c710)")?;
-
-        assert_eq!(instruction, Instruction::new(Direction::Right, 6));
+    fn test_parse_instruction_v1() -> Result<()> {
+        assert_eq!(
+            parse_instruction_v1("R 6 (#70c710)")?.1,
+            Instruction::new(Direction::Right, 6)
+        );
 
         Ok(())
     }
 
     #[test]
-    fn test_parse_instruction_v2() -> anyhow::Result<()> {
-        let (_, instruction) = parse_instruction_v2("R 6 (#70c710)")?;
+    fn test_parse_instruction_v2() -> Result<()> {
+        assert_eq!(
+            parse_instruction_v2("R 6 (#70c710)")?.1,
+            Instruction::new(Direction::Right, 461937,)
+        );
 
-        assert_eq!(instruction, Instruction::new(Direction::Right, 461937,));
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_direction_v1() -> Result<()> {
+        assert_eq!(parse_direction_v1("R")?.1, Direction::Right);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_direction_v2() -> Result<()> {
+        assert_eq!(parse_direction_v2("0")?.1, Direction::Right);
+
+        assert!(parse_direction_v2("5").is_err());
 
         Ok(())
     }
